208be084506d623ae536fbc99b4adf2567be084e
devhmv2 <teamdevhm2@gmail.com>
2020-10-29 14:22:53 +0700

    a4

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,5 +1,21 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+ class Dashboard extends CI_controller
+ {
+     public function __construct()
+     {
+         parent::__construct();
+         
+     }
+      public function index()
+      {
+            
+          $data ['template'] ='template/index';
+          $this->load->view('layout/index');
+          
+      }
+     
+     
+ }
 
-
-?>
